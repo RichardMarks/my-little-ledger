37c542cfdbf25a8b22a4de39f5b27d895e3e066f
Richard Marks <ccpsceo@gmail.com>
2017-09-18 00:57:55 -0500

    initial cli implemented

--- my-little-ledger.go
+++ my-little-ledger.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -102,49 +104,213 @@ func formatTimestamp(timestamp int64) string {
 	return time.Unix(timestamp, 0).Format("01/02/2006 03:04:05 PM MST")
 }
 
+func getCommandLine() (int, []string) {
+	argc := len(os.Args) - 1
+	argv := os.Args[1:]
+	return argc, argv
+}
+
+func showHelp() {
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	os.Exit(0)
+}
+
+// WorkspaceConfig represents the configuration for the ledger workspace
+type WorkspaceConfig struct {
+	ActiveAccount string `json:"activeAccount"`
+}
+
+func createPath(userPath string) error {
+	if _, err := os.Stat(userPath); os.IsNotExist(err) {
+		err = os.MkdirAll(userPath, os.ModePerm)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+	}
+	return nil
+}
+
+func createFile(fileName string, fileBytes []byte) error {
+	err := ioutil.WriteFile(fileName, fileBytes, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	return nil
+}
+
+func getWorkspacePath() string {
+	absolutePathToCurrentDirectory, _ := filepath.Abs("./")
+	workspacePath := path.Join(absolutePathToCurrentDirectory, ".ledger/")
+	return workspacePath
+}
+
+func getWorkspaceConfigPath() string {
+	workspacePath := getWorkspacePath()
+	configFilePath := path.Join(workspacePath, "config.json")
+	return configFilePath
+}
+
+func createDefaultWorkspaceConfiguration() WorkspaceConfig {
+	config := WorkspaceConfig{ActiveAccount: "default"}
+	return config
+}
+
+func (config *WorkspaceConfig) save(fileName string) error {
+	configFileBytes, err := json.Marshal(config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	err = createFile(fileName, configFileBytes)
+	return err
+}
+
+func (config *WorkspaceConfig) load(fileName string) error {
+	configFileBytes, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	err = json.Unmarshal(configFileBytes, &config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	return nil
+}
+
+func initializeWorkspace() error {
+	workspacePath := getWorkspacePath()
+	createPath(workspacePath)
+
+	configFilePath := path.Join(workspacePath, "config.json")
+	config := createDefaultWorkspaceConfiguration()
+	config.save(configFilePath)
+
+	defaultAccountPath := path.Join(workspacePath, "default")
+	createPath(defaultAccountPath)
+
+	defaultAccount := createAccount(0)
+	defaultAccountFilePath := path.Join(defaultAccountPath, "data.json")
+	defaultAccount.saveToFile(defaultAccountFilePath)
+
+	fmt.Println("\n*** initialized ledger workspace")
+	return nil
+}
+
+func selectActiveAccount(accountName string) {
+	configFilePath := getWorkspaceConfigPath()
+	config := WorkspaceConfig{}
+	config.load(configFilePath)
+	config.ActiveAccount = accountName
+	config.save(configFilePath)
+}
+
+func createNewAccount(accountName string) {
+	workspacePath := getWorkspacePath()
+	accountPath := path.Join(workspacePath, accountName)
+	createPath(accountPath)
+	account := createAccount(0)
+	accountFilePath := path.Join(accountPath, "data.json")
+	account.saveToFile(accountFilePath)
+	selectActiveAccount(accountName)
+}
+
+func unknownAction(action string) {
+	fmt.Printf("Unknown Action %s\n", action)
+	os.Exit(-1)
+}
+
 func main() {
 	fmt.Println("My Little Ledger v1.0")
+	argc, argv := getCommandLine()
 
-	account := createAccount(0)
-
-	err := account.readFromFile("my-account.json")
-	if err != nil {
-		fmt.Println("There was a critical error reading the account database!")
-		fmt.Println(err.Error())
-		os.Exit(1)
+	if argc >= 1 {
+		action := strings.ToLower(strings.Trim(argv[0], " \n"))
+		switch action {
+		case "init":
+			initializeWorkspace()
+		case "new":
+			if argc >= 2 {
+				accountName := strings.ToLower(strings.Trim(argv[1], " \n"))
+				createNewAccount(accountName)
+			} else {
+				fmt.Println("Unable to create account. Missing Required Account Name")
+				os.Exit(-1)
+			}
+		// case "ls": listTransactions()
+		// case "shell": startInteractiveLedger()
+		default:
+			unknownAction(action)
+		}
+	} else {
+		showHelp()
 	}
 
-	// accountFileBytes := []byte(`{ "startBalance": 0.00, "balance": 0.00, "transactions": [] }`)
+	// usage
+	// $ appname
+	//    show help
+	// $ appname init
+	//    create .ledger/config.json
+	// $ appname new accountname
+	//    create .ledger/accountname/data.json
+	//    write `"activeAccount":accountname`
+	// $ appname ls
+	//    list transactions in active account
+	// $ appname shell
+	//    start the interactive shell to create, update, and delete transactions
 
-	fmt.Printf("Starting Balance:\t%s\n", formatMoney(account.StartBalance))
+	// createAccountFlag := flag.Bool("createaccount", false, "specifies to create an account")
+	// argv := flag.Args()
+	// fmt.Println(*createAccountFlag)
 
-	// account.deposit(fToMoney(100.0))
-	// account.withdraw(fToMoney(30))
+	fmt.Print(argc, argv)
 
-	fmt.Printf("Balance:\t\t%s\n", formatMoney(account.Balance))
-	hr := strings.Repeat("-", 90)
+	// account := createAccount(0)
 
-	fmt.Println("Transactions: ")
-	for i := 0; i < len(account.Transactions); i++ {
-		transaction := account.Transactions[i]
-		income := transaction.Income
-		expense := transaction.Expense
-		balance := transaction.Balance
-		ts := transaction.Timestamp
-
-		fmt.Printf("%04d: %s IN %s OUT %s BAL - %s\n%s\n", i, formatMoney(income), formatMoney(expense), formatMoney(balance), formatTimestamp(ts), hr)
-	}
-
-	// accountFileBytes, err := json.Marshal(account)
+	// err := account.readFromFile("my-account.json")
 	// if err != nil {
-	// 	log.Print(err)
-	// 	return
+	// 	fmt.Println("There was a critical error reading the account database!")
+	// 	fmt.Println(err.Error())
+	// 	os.Exit(1)
 	// }
 
-	// err = ioutil.WriteFile("account.json", accountFileBytes, 0644)
-	// if err != nil {
-	// 	log.Print(err)
-	// 	return
+	// // accountFileBytes := []byte(`{ "startBalance": 0.00, "balance": 0.00, "transactions": [] }`)
+
+	// fmt.Printf("Starting Balance:\t%s\n", formatMoney(account.StartBalance))
+
+	// // account.deposit(fToMoney(100.0))
+	// // account.withdraw(fToMoney(30))
+
+	// fmt.Printf("Balance:\t\t%s\n", formatMoney(account.Balance))
+	// hr := strings.Repeat("-", 90)
+
+	// fmt.Println("Transactions: ")
+	// for i := 0; i < len(account.Transactions); i++ {
+	// 	transaction := account.Transactions[i]
+	// 	income := transaction.Income
+	// 	expense := transaction.Expense
+	// 	balance := transaction.Balance
+	// 	ts := transaction.Timestamp
+
+	// 	fmt.Printf("%04d: %s IN %s OUT %s BAL - %s\n%s\n", i, formatMoney(income), formatMoney(expense), formatMoney(balance), formatTimestamp(ts), hr)
 	// }
+
+	// // accountFileBytes, err := json.Marshal(account)
+	// // if err != nil {
+	// // 	log.Print(err)
+	// // 	return
+	// // }
+
+	// // err = ioutil.WriteFile("account.json", accountFileBytes, 0644)
+	// // if err != nil {
+	// // 	log.Print(err)
+	// // 	return
+	// // }
 
 }
